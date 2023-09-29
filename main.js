78275f1245a93b35213f6760d6dd58cb145d10e3
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2023-09-29 10:23:29 -0300

    OK

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ alert('POM');
 
 document.querySelector(‘.tecla_pom’);
 document.querySelector(‘#som_tecla_pom’).play();
+
+function tocaSomPom(){
+    document.querySelector(‘#som_tecla_pom’).play();
+    }

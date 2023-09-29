92429c03d1cac9de9d1c5d5285ab40f4d991e278
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2023-09-29 10:32:10 -0300

    .

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ alert('POM');
 
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play();
-    document.querySelector('.tecla_pom').onclick = tocaSomPom;
     }
+    document.querySelector('.tecla_pom').onclick = tocaSomPom;

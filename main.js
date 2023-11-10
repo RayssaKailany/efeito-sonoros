d197282eba74e349b68e9923e45a3a011cafe10d
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2023-11-10 11:13:51 -0300

    ok

--- main.js
+++ main.js
@@ -10,6 +10,6 @@ for(let contador = 0; contador < istaDeTeclas.length; contador++){
     const efeito = teclas.classlist[1]; //chama o 2 item classe individual do som
     const idAudio = `"#som_" ${efeito}`; //utiliza a junção template string ${ }
     teclas.onclick = function(){       //função anônima auxiliar para chamar tocaSom
-        tocaSomPom(idAudio);         //chamada da função tocaSom com parâmetro idAudio
+        tocaSom(idAudio);         //chamada da função tocaSom com parâmetro idAudio
     }
 }

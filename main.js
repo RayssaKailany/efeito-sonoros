13ea3e5b113ba0a39d11a1961cfccf42dcb51fdc
Rayssa Kailany <102701353+RayssaKailany@users.noreply.github.com>
2023-11-10 11:00:37 -0300

    .

--- main.js
+++ main.js
@@ -1,18 +1,15 @@
 //Declaração/criação da função TocaSom
-function TocaSomPom(){
-    document.querySelector('#som_tecla_pom'). play();
+function TocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio). play();
 }
-//chamda ou invocação da função tocaSomPom ao clicar no botão
-document.querySelector('tecla_pom').onclick = TocaSomPom;
+//criando a referências constantes listaDeTeclas e buscando a classe coletiva .tecla
+const listaDeTeclas = document.querySelectorAll(`.tecla`);
 
-let contador = 0;
-whitle(contador < istaDeTeclas.length){
+for(let contador = 0; contador < istaDeTeclas.length; contador++){
     const teclas = listaDeTeclas[contador]
     const efeito = teclas.classlist[1];
     const idAudio = `"#som_" ${efeito}`;
     teclas.onclick = function(){
         tocaSomPom(idAudio);
     }
-    contador = contador + 1;
-    console.long(contador);
 }
